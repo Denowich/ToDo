29b8755fa4da4ad68bacfc1cc26982fbcbb3bbd3
Denowich <denowich@gmail.com>
2025-12-20 00:20:03 +0200

    added base app

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import './styles'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import App from './App.jsx'
+
+
+const rootEl = document.getElementById('root');
+const reactRoot = createRoot(rootEl);
+reactRoot.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
